b3959253a057d0e5e9bed6ffd4729f53b86ec188
lionoflanta <stefanodipierro@live.com>
2023-06-14 11:24:36 +0200

    commit

--- flask_blog_template/app/main/forms.py
+++ flask_blog_template/app/main/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired
 
 class SearchForm(FlaskForm):
@@ -8,3 +8,9 @@ class SearchForm(FlaskForm):
     """
     query = StringField('Search', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+# Login form 
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
